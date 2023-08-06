544349a20d77590c4efc9c7ea46dc83704688d91
Ojas Aklecha <ojasaklechayt@gmail.com>
2023-08-06 12:56:58 +0530

    ENV Updated

--- src/components/Connect/connect.tsx
+++ src/components/Connect/connect.tsx
@@ -11,7 +11,7 @@ import "./connect.css";
 
 const ComposeEmailButton = () => {
     const composeEmail = () => {
-        const emailId = 'recipient@example.com';
+        const emailId = process.env.GMAIL;
         const subject = 'Hello!'; // You can set a predefined subject if needed
 
         // Create the mailto URL with the predefined email ID and subject
